0a58636c1330ec7548b073f78028259956e7d0c2
Dejan Slamkov <slamkovDejan@hotmail.com>
2020-07-08 15:35:38 +0200

    Add forum-management module

---
+++ forum-management/src/main/java/mk/ukim/finki/emt/forum/forummanagement/ForumManagementApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.forum.forummanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumManagementApplication.class, args);
+    }
+
+}
---
+++ forum-management/src/test/java/mk/ukim/finki/emt/forum/forummanagement/ForumManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.forum.forummanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
